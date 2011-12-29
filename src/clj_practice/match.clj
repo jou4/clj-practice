03eaf7a0a0fd09c285e685f895a01e1bbd23c9d4
K. Kamitsukasa <jou4.kz@gmail.com>
2011-12-29 14:45:41 +0900

    add match

--- src/clj_practice/match.clj
+++ src/clj_practice/match.clj
@@ -1,22 +1,18 @@
 (ns clj-practice.match)
 
 (def re1 #"\d{3}\-\d{4}")
-
 (re-find re1 "123-4567")
 (re-find re1 "123 - 4567")
-
 (re-find re1 "123-4567 234-5678")
 (re-seq re1 "123-4567 234-5678")
 
-(import '[java.util.regex Pattern])
 
-
-(def re2 (Pattern/compile "\\d{3}\\-\\d{4}" Pattern/CASE_INSENSITIVE))
-
+(def re2 (re-pattern "\\d{3}\\-\\d{4}"))
 (re-find re2 "123-4567")
 (re-find re2 "123 - 4567")
 
 
-(def re3 #"\d{3}")
-; (re-groups (re-matcher re3 "123-4567"))
-
+(def re3 #"(\d{3})\-(\d{4})")
+(re-find (re-matcher re3 "123-4567"))
+(re-matches re3 "123-4567")
+(re-matches re3 " 123-4567 ")
