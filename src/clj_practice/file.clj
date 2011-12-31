2afcd1f642ccac04bd94ae97b4148e6d9215b7bf
K. Kamitsukasa <jou4.kz@gmail.com>
2011-12-31 17:48:36 +0900

    add file

--- src/clj_practice/file.clj
+++ src/clj_practice/file.clj
@@ -2,4 +2,12 @@
 
 (require '[clojure.java.io :as io])
 
+; (doseq [m (.getMethods java.io.File)] (println m))
+
 (doseq [f (file-seq (io/file "lib"))] (println (.getName f)))
+
+(map #(.getName %)
+     (filter #(.isDirectory %) (.listFiles (io/file "lib"))))
+
+(map #(.getName %)
+     (filter #(.isFile %) (.listFiles (io/file "lib"))))
